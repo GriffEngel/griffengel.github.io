2f1e3588cc2eda0d74ecd983bf2f89703aec5ec3
CoreSNK <gnengel07@gmail.com>
2024-03-19 10:33:03 -0500

    Adding optimization changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    mode: 'jit',
+    purge: ['./src/**/*.{js,jsx,ts,tsx,html}'],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
 
