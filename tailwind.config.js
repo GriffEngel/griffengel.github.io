abafde9cd425fd2cce249bb727bd8242dc3be934
CoreSNK <gnengel07@gmail.com>
2024-04-01 12:51:39 -0500

    minified index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ],
     content: ['./src/**/*.{html,js,jsx,ts,tsx}', 'index.html'],
     theme: {
         extend: {},
